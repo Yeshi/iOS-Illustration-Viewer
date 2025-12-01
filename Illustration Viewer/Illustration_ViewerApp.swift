f5ef9b7dece3d27061028dcc240d9aa43c50ad90
Yeshi <yoshikema@gmail.com>
2025-12-01 17:18:15 +0900

    Initial Commit

---
+++ Illustration Viewer/Illustration_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Illustration_ViewerApp.swift
+//  Illustration Viewer
+//
+//  Created by Yeshi on 2025/12/01.
+//
+
+import SwiftUI
+
+@main
+struct Illustration_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
